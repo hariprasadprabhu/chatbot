fa7d8f299c33ce906d31e7bb2eb92461817fe351
Hariprasad <lshariprasadp@gmail.com>
2024-07-07 11:00:41 +0530

    Basic set up

---
+++ src/main/java/com/chatbot/ProChatApplication.java
@@ -0,0 +1,13 @@
+package com.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProChatApplication.class, args);
+	}
+
+}
